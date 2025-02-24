9b8bce0e85f4d1f54e99d96c2647b290c999ab80
leemyunglyul <leemyl@naver.com>
2025-02-24 06:22:20 -0800

    byte_stream success

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,42 +12,49 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity)
+    : buffer(""), _capacity(capacity), _bytes_written(0), _bytes_read(0), isInputEnded(false) {}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t len_write = min(data.size(), remaining_capacity());
+    buffer.append(data.substr(0, len_write));
+    _bytes_written += len_write;
+    return len_write;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    return buffer.substr(0, min(len, buffer.size()));
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+    size_t len_pop = min(len, buffer.size());
+    buffer.erase(0, len_pop);
+    _bytes_read += len_pop;
+ }
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    string result = peek_output(len);
+    pop_output(len);
+    return result;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() { isInputEnded = true; }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return isInputEnded; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return buffer.size(); }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return buffer.empty(); }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return isInputEnded && buffer.empty(); }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _bytes_written; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _bytes_read; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return _capacity - buffer.size(); }
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -17,6 +17,12 @@ class ByteStream {
     // that's a sign that you probably want to keep exploring
     // different approaches.
 
+    std::string buffer;
+    size_t _capacity;
+    size_t _bytes_written;
+    size_t _bytes_read;
+    bool isInputEnded;
+
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
   public:
