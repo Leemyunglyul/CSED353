ebd05073ffcd55ce95b582d595251360afce3401
leemyunglyul <leemyl@naver.com>
2025-02-24 05:59:10 -0800

    feat: webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -17,8 +17,27 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+    TCPSocket socket;
+
+    Address dest(host, "80");
+
+    socket.connect(dest);
+
+    string request = "GET " + path + " HTTP/1.1\r\n" +
+                     "Host: " + host + "\r\n"
+                     "Connection: close\r\n\r\n"; 
+
+    socket.write(request);
+
+    socket.shutdown(SHUT_WR);
+
+    while(!socket.eof()){
+        string response = socket.read();
+        cout << response;
+    }
 }
 
 int main(int argc, char *argv[]) {
