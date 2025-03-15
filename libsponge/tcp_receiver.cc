9e9e40fafc8212618b9e9f1ff5af4ba276f8932c
leemyunglyul <leemyl@naver.com>
2025-03-15 04:50:33 -0700

    assn2: maybe final

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,7 +11,26 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader hdr = seg.header();
+
+    if (!_syn_flag) {
+        if (!hdr.syn) return;
+        _isn = hdr.seqno;
+        _syn_flag = true;
+    }
+
+    const uint64_t checkpoint = _reassembler.stream_out().bytes_written() + 1;
+    const uint64_t absolute_seqno = unwrap(hdr.seqno, _isn, checkpoint);
+
+    const uint64_t stream_idx = absolute_seqno - 1 + hdr.syn;
+
+    _reassembler.push_substring(seg.payload().copy(), stream_idx, hdr.fin);
+
+    if (hdr.fin || _fin_flag) {
+        _fin_flag = true;
+        if (_reassembler.unassembled_bytes() == 0)
+            _reassembler.stream_out().end_input();
+    }
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
@@ -19,6 +38,8 @@ optional<WrappingInt32> TCPReceiver::ackno() const {
     if (!_syn_flag) return nullopt;
 
     uint64_t index = _reassembler.output_idx() + 1;
+    if (_reassembler.stream_out().input_ended())
+        index++;
     return wrap(index, _isn);
 }
 
